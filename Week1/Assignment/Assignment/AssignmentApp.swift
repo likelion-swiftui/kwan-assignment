56f3ccb8ef61b61235f74844192046f4524c8b6f
김응관 <40496065+alpha-kwhn@users.noreply.github.com>
2022-10-10 17:11:53 +0900

    Assignment

---
+++ Week1/Assignment/Assignment/AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentApp.swift
+//  Assignment
+//
+//  Created by 김응관 on 2022/10/10.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
