e2d6d2d6ce659f8078147a058dc2b6ecbdf0a37a
김응관 <40496065+alpha-kwhn@users.noreply.github.com>
2022-10-13 01:09:30 +0900

    Add files via upload

---
+++ Week2/Assignment/Week2App.swift
@@ -0,0 +1,17 @@
+//
+//  Week2App.swift
+//  Week2
+//
+//  Created by 김응관 on 2022/10/11.
+//
+
+import SwiftUI
+
+@main
+struct Week2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
